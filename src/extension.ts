9c59d7f185b840355558b54fcaf84c3975db4bf9
Allain Lalonde <allain.lalonde@gmail.com>
2018-05-27 22:59:17 -0400

    bumping to latest piff

--- src/extension.ts
+++ src/extension.ts
@@ -4,43 +4,50 @@ import * as vscode from 'vscode';
 import * as piff from 'piff'
 import * as fs from 'fs'
 
+function compilePiff(document) {
+    const piffCode = document.getText()
+    const phpCode = piff.transpile(piffCode)
+    const phpPath = document.uri.fsPath.replace(/[.]piff$/, '.php')
+
+    fs.writeFile(phpPath, phpCode, {
+        encoding: 'utf-8'
+    }, err => {
+        if (err)
+            console.error(err)
+    })
+
+    return phpPath
+}
+
+
 export function activate(context: vscode.ExtensionContext) {
     console.log('registering piff')
 
-    function compilePiff(document) {
-        const piffCode = document.getText()
-        const phpCode = piff.transpile(piffCode)
-        const phpPath = document.uri.fsPath.replace(/[.]piff$/, '.php')
-        fs.writeFile(phpPath, phpCode, {
-            encoding: 'utf-8'
-        }, err => {
-            console.error(err)
-        })
-
-        return phpPath
-    }
-
     vscode.workspace.onDidSaveTextDocument((document: vscode.TextDocument) => {
-        compilePiff(document)
+        if (document.languageId === 'piff') {
+            compilePiff(document)
+        }
     })
 
-    vscode.languages.registerDocumentFormattingEditProvider('piff', {
-        provideDocumentFormattingEdits(document: vscode.TextDocument): vscode.TextEdit[] {
-            const phpPath = document.uri.fsPath.replace(/[.]piff$/, '.php')
-            const compiled = compilePiff(document)
-            const piffCode = document.getText()
-            const firstLine = document.lineAt(0);
-            const lastLine = document.lineAt(document.lineCount - 1);
-            const fullRange = new vscode.Range(0,
-                firstLine.range.start.character,
-                document.lineCount - 1,
-                lastLine.range.end.character);
+    vscode.languages.registerDocumentFormattingEditProvider(
+        { scheme: 'file', language: 'piff' },
+        {
+            provideDocumentFormattingEdits(document: vscode.TextDocument): vscode.TextEdit[] {
+                const phpPath = document.uri.fsPath.replace(/[.]piff$/, '.php')
+                const compiled = compilePiff(document)
+                const piffCode = document.getText()
+                const firstLine = document.lineAt(0);
+                const lastLine = document.lineAt(document.lineCount - 1);
+                const fullRange = new vscode.Range(0,
+                    firstLine.range.start.character,
+                    document.lineCount - 1,
+                    lastLine.range.end.character);
 
-            const formatted = piff.format(piffCode);
+                const formatted = piff.format(piffCode);
 
-            return [vscode.TextEdit.replace(fullRange, formatted)];
-        }
-    });
+                return [vscode.TextEdit.replace(fullRange, formatted)];
+            }
+        });
 
 }
 
